b37e772c4b3f17d27c77965a65b5c7f169f0b1a2
kamiyamak <kamiyamak@istyle.co.jp>
2022-10-27 23:23:52 +0900

    fastly-nextjs

---
+++ app/sayHello.ts
@@ -0,0 +1,10 @@
+function genBirdsInfo(name: string): string[] {
+    return name.split(',')
+}
+
+function singBirds(birdInfo: (x:string) => string[]): string {
+    return birdInfo('hato, kiji')[0] + ' piyo piyo'
+}
+
+console.log(singBirds(genBirdsInfo))
+console.log(singBirds('dobato'))
---
+++ pages/index.js
@@ -0,0 +1,5 @@
+export default function Index() {
+      return (
+        <div>Hey, World!</div>
+      );
+    };
